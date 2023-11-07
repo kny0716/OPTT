4ed4193040c6cf720922bde37fb5e3b1ea216e49
Nayeon Kim <kny020716@naver.com>
2023-11-07 18:46:32 +0900

    김나연 이미지 및 js, 폰트, scss

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Main from "./components/section/Main";
+
+const Home = lazy(() => import("./pages/Home"));
+const Survey = lazy(() => import("./pages/Survey"));
+const Result = lazy(() => import("./pages/Result"));
+const Explain = lazy(() => import("./pages/Explain"));
+const Login = lazy(() => import("./pages/Login"));
+const Register = lazy(() => import("./pages/Register"));
+const Not = lazy(() => import("./pages/Not"));
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Suspense fallback={<Main />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/survey" element={<Survey />} />
+          <Route path="/result" element={<Result />} />
+          <Route path="/explain" element={<Explain />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/*" element={<Not />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  );
 };
 
 export default App;
