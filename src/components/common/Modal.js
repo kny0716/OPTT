ea4f474a461417b4a54999c54a29844ad94b1edc
Nayeon Kim <kny020716@naver.com>
2023-11-14 18:11:19 +0900

    김나연 로그인,회원가입 UI수정, 모달창, 설문페이지 UI

---
+++ src/components/common/Modal.js
@@ -0,0 +1,21 @@
+import Button from "./Button";
+export default function Modal({ close }) {
+  return (
+    <div className="wrapper">
+      <div className="overlay" onClick={close}></div>
+      <div className="modal__form">
+        <div className="profile">
+          <img src="/img/profile.svg"></img>
+          <p>account</p>
+        </div>
+        <div className="contents">
+          <Button to="/result" className="contents__btn">
+            나의 테스트 결과
+          </Button>
+          <div className="contents__line"></div>
+          <p className="contents__logout">로그아웃</p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/Survey.jsx
+++ src/pages/Survey.jsx
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Main from "../components/section/Main";
+import SurveyForm from "../components/common/SurveyForm";
 
 const Survey = () => {
-  return <Main>Survey</Main>;
+  const [question_number, set_question_number] = useState(1);
+  const handleAnswerChange = () => {
+    if (question_number + 1 === 12) {
+      window.location.href = "/result";
+    } else {
+      set_question_number(question_number + 1);
+    }
+  };
+  return (
+    <Main>
+      <SurveyForm
+        number={question_number}
+        onClick={() => handleAnswerChange()}
+      />
+    </Main>
+  );
 };
 
 export default Survey;
