4b2d4149ebdb610fc8fb44b3988be40e578c4a62
Sminp <0102262@gmail.com>
2023-11-28 20:59:22 +0900

    박소민 aws DB 업로드

--- src/server/user.js
+++ src/server/user.js
@@ -2,7 +2,7 @@ var mysql = require("mysql");
 var ejs = require("ejs");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "database-1.cwazjrr2iqe5.ap-northeast-2.rds.amazonaws.com",
   user: "root",
   password: "ckdmsdn330!!",
   database: "optt",
