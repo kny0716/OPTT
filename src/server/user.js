9d3ad8c91149cf10b9739ae5840fa6e82b3f6d33
Sminp <0102262@gmail.com>
2023-11-30 19:20:04 +0900

    박소민 rds... 코파일럿 겁나화나

--- src/server/user.js
+++ src/server/user.js
@@ -2,7 +2,7 @@ var mysql = require("mysql");
 var ejs = require("ejs");
 
 var connection = mysql.createConnection({
-  host: "database-1.cwazjrr2iqe5.ap-northeast-2.rds.amazonaws.com",
+  host: "database-1.c7l3lp7npccg.ap-northeast-2.rds.amazonaws.com",
   user: "root",
   password: "ckdmsdn330!!",
   database: "optt",
