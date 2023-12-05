baaa904c0967138e9dfd7943d474a3002d6014b2
Sminp <0102262@gmail.com>
2023-12-05 18:08:07 +0900

    박소민 aws 오류 수정

--- src/server/user.js
+++ src/server/user.js
@@ -2,7 +2,7 @@ var mysql = require("mysql");
 // var ejs = require("ejs");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "127.0.0.1",
   user: "root",
   password: "ckdmsdn330!!",
   database: "optt",
